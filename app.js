c39bb0a81a9d69fa99842ed7a307ac168b35c707
danimaron <marongiudan@gmail.com>
2024-02-19 15:48:44 +0000

    implemented GET /api

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const getTopics = require("./controllers/topics.controller");
+const getEndpoints = require("./controllers/endPoints.controller");
 const app = express();
 
 app.get('/api/topics', getTopics)
+app.get('/api',getEndpoints)
+
 
 app.use((err, req, res, next) => {
+    console.log(err);
   res.status(500).send({ msg: "An error has occured" });
 });
 
---
+++ models/endPoints.model.js
@@ -0,0 +1,16 @@
+const db = require("../db/connection");
+const fs = require('fs/promises')
+const path = require("path");
+
+const endpointsPath = path.join(__dirname, "../endpoints.json");
+
+function fetchEndpoints() {
+
+    return fs.readFile(endpointsPath, 'utf8')
+        .then((endpoints) => {
+        return JSON.parse(endpoints)
+    })
+
+}
+
+module.exports = fetchEndpoints;
