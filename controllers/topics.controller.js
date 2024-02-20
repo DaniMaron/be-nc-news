3a2eae5551cd213366c4d6fc787c4c0a23555c6f
danimaron <marongiudan@gmail.com>
2024-02-20 11:09:50 +0000

    added .catch to file as it was previously commented out

--- controllers/topics.controller.js
+++ controllers/topics.controller.js
@@ -5,7 +5,7 @@ function getTopics(req, res, next) {
     .then((topics) => {
       res.status(200).send({ topics });
     })
-    // .catch(next);
+    .catch(next);
 }
 
 module.exports = getTopics;
