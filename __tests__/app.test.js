971ab5d1757d7605c41365760a8d8bb3ea7e0fea
danimaron <marongiudan@gmail.com>
2024-02-20 14:15:09 +0000

    implemented GET /api/articles

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -49,6 +49,7 @@ describe("GET api/articles/:article_id", () => {
       .expect(200)
       .then(({ body }) => {
         expect(typeof body).toBe("object");
+
         expect(Object.keys(body[0]).includes("author")).toBe(true);
         expect(Object.keys(body[0]).includes("title")).toBe(true);
         expect(Object.keys(body[0]).includes("article_id")).toBe(true);
@@ -57,7 +58,7 @@ describe("GET api/articles/:article_id", () => {
         expect(Object.keys(body[0]).includes("created_at")).toBe(true);
         expect(Object.keys(body[0]).includes("votes")).toBe(true);
         expect(Object.keys(body[0]).includes("article_img_url")).toBe(true);
-        console.log(body[0]);
+
         expect(body[0].author).toBe('icellusedkars');
         expect(body[0].title).toBe("Eight pug gifs that remind me of mitch");
         expect(body[0].article_id).toBe(3);
@@ -90,37 +91,45 @@ describe("GET api/articles/:article_id", () => {
   });
 });
 
-xdescribe("GET api/articles", () => {
-  test("Status-200: responds with an array containing all articles", () => {
+describe("GET api/articles", () => {
+  test("Status-200: responds with an array containing all articles ordered by date in descending order", () => {
     return request(app)
       .get("/api/articles")
       .expect(200)
       .then(({ body }) => {
         expect(Array.isArray(body)).toBe(true);
+        expect(body.length).toBe(13)
         expect(Object.keys(body[0]).includes("author")).toBe(true);
         expect(Object.keys(body[0]).includes("title")).toBe(true);
         expect(Object.keys(body[0]).includes("article_id")).toBe(true);
-        expect(Object.keys(body[0]).includes("body")).toBe(true);
         expect(Object.keys(body[0]).includes("topic")).toBe(true);
         expect(Object.keys(body[0]).includes("created_at")).toBe(true);
         expect(Object.keys(body[0]).includes("votes")).toBe(true);
         expect(Object.keys(body[0]).includes("article_img_url")).toBe(true);
-      });
+        
+        expect(body[0].author).toBe("icellusedkars");
+        expect(body[0].title).toBe("Eight pug gifs that remind me of mitch");
+        expect(body[0].article_id).toBe(3);
+        expect(body[0].topic).toBe("mitch");
+        expect(body[0].created_at).toBe("2020-11-03T09:12:00.000Z");
+        expect(body[0].votes).toBe(0);
+        expect(body[0].article_img_url).toBe(
+          "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700"
+          )
+          
+          expect(body).toBeSortedBy("created_at", { descending: true });
+        });
   });
-  test("Status 404: responds with an appropriate error when given an article Id that does not exist", () => {
-    return request(app)
-      .get("/api/articles/999999")
-      .expect(404)
-      .then(({ body: { msg } }) => {
-        expect(msg).toBe("Article not found");
-      });
-  });
-  test("Status 400: responds with an appropriate error when given an article Id that is invalid", () => {
-    return request(app)
-      .get("/api/articles/forklift")
-      .expect(400)
-      .then(({ body: { msg } }) => {
-        expect(msg).toBe("Invalid input");
-      });
-  });
+  
+      test('Status 200: responds with an array of articles each containing a comments_count key with the total value of comments for said article', () => {
+        return request(app)
+          .get("/api/articles")
+          .expect(200)
+          .then(({ body }) => {
+            expect(body[0].comment_count).toBe('2')
+            expect(body[1].comment_count).toBe('1')
+            expect(body[12].comment_count).toBe('0')
+            expect(body[6].comment_count).toBe('11')
+          })
+        })
 });
